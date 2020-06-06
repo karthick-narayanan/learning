a38035d4dd8f661e80c1f8f1ab9e57e66412ffa4
Karthick Narayanan (knaraya2) <knaraya2@cisco.com>
2020-06-06 21:07:06 +0530

    Final Working changes.

--- nqueen.cpp
+++ nqueen.cpp
@@ -1,21 +1,21 @@
 /*
 This program is to find the number of different ways n queesn can be placed in the board.
-*
+*/
 #include <bits/stdc++.h>
 
-using namespace std;
 int count = 0;
-int findn(int row , int n, std::vector<int> col, std::vector<int> diag1, std::vector<int> diag2) {
+
+void findn(int row , int n, std::vector<int> &col, std::vector<int> &diag1, std::vector<int> &diag2) {
   if(row == n) {
     count++;
+    return;
   }
-
   for(int i=0; i<n ;i++) {
-    if(col[i] || diag1[i+row] || diag2[row-i+(n-1)])){
+    if(col[i] || diag1[i+row] || diag2[row-i+(n-1)]){
       continue;
     }
     col[i] = diag1[i+row] = diag2[row-i+(n-1)] = 1;
-    findn(row+1, n, col, diag1, diag2)
+    findn(row+1, n, col, diag1, diag2);
     col[i] = diag1[i+row] = diag2[row-i+(n-1)] = 0;
   }
 }
@@ -23,13 +23,16 @@ int findn(int row , int n, std::vector<int> col, std::vector<int> diag1, std::ve
 
 int main() {
   int n;
-  vector <int> col;
-  vector <int> diag1;
-  vector <int> diag2;
 
 
-  count << "n queen problem , enter n (nxn) "
-  cin >> n;
+  std::cout << "n queen problem , enter n (nxn) ";
+  std::cin >> n;
+
+  std::vector <int> col(n,0);
+  std::vector <int> diag1(n*2,0);
+  std::vector <int> diag2(n*2,0);
+
   findn(0, n, col, diag1, diag2);
-  cout << count;
+  std::cout<< count <<std::endl;
+  return 0;
 }
