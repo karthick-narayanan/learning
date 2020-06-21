1e907334e82148ce45d431bfd65127ac68e6cf92
Karthick Narayanan (knaraya2) <knaraya2@cisco.com>
2020-06-21 12:12:42 +0530

    scheduling

---
+++ problemsolving/6.Scheduling.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+bool compare(const std::pair<int,int> &a, const std::pair <int,int> &b) {
+  return a.second<b.second;
+}
+
+int main() {
+
+    std::vector< std::pair<int,int> > v;
+    unsigned int t,s,e;
+    int count = 1, last_end = 0;
+    srand((unsigned) time(0));
+
+    std::cin >> t;
+    while(t--) {
+      s = rand()%100;
+      e = s+(rand()%100);
+      v.push_back(std::make_pair(s,e));
+      std::cout<<s<<","<<e<<" ";
+    }
+    std::cout << "\n";
+    std::sort(v.begin(),v.end(),compare);
+    last_end = v[0].second;
+    for(int i=1; i<v.size(); i++) {
+      if(last_end < v[i].first) {
+        count++;
+        last_end = v[i].second;
+      }
+    }
+
+    std::cout << count << "\n";
+
+
+
+}
