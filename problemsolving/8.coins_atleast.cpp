345664a9411eb2a441dc635fd21b47e153de4061
Karthick Narayanan (knaraya2) <knaraya2@cisco.com>
2020-06-28 21:16:01 +0530

    commit with printing the list of coins.

--- problemsolving/8.coins_atleast.cpp
+++ problemsolving/8.coins_atleast.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-int solve_m(std::vector<int> &v, std::vector<int> &coins,int m){
+int solve_m(std::vector<int> &v, std::vector<int> &coins, std::vector<int> &print_coins, int m){
 
   if(v[m] != INT_MAX) {
     return v[m];
@@ -8,7 +8,11 @@ int solve_m(std::vector<int> &v, std::vector<int> &coins,int m){
 
   for(auto c : coins) {
     if(m-c){
-      v[m] = std::min(v[m], solve_m(v, coins, m-c)+1);
+      int ct = std::min(v[m], solve_m(v, coins, print_coins, m-c)+1);
+      if(ct<v[m]) {
+        v[m] = ct;
+        print_coins[m] = c;
+      }
     }
   }
   return v[m];
@@ -17,17 +21,20 @@ int solve_m(std::vector<int> &v, std::vector<int> &coins,int m){
 int main(){
   std::vector<int> v;
   std::vector<int> coins;
+  std::vector<int> print_coins;
   int n,m;
   std::cout<<"no of coins :";
   std::cin>>n;
   std::cout<<"find m wasy for:";
   std::cin>>m;
   v.assign(m+1, INT_MAX);
+  print_coins.assign(m+1, -1);
   while(n--){
     int t;
     std::cin>>t;
     coins.push_back(t);
     v[t] = 1;
+    print_coins[t] = t;
   }
 
   /* we are calculating for all possible combiatnions. lets calculate only what we want.
@@ -38,6 +45,10 @@ int main(){
       }
     }
   }*/
-  solve_m(v, coins, m);
-  std::cout<<v[m];
+  solve_m(v, coins, print_coins, m);
+  std::cout<<v[m]<<"\n";
+  while(print_coins[m] > 0) {
+    std::cout<<print_coins[m]<< " " ;
+    m = m - print_coins[m];
+  }
 }
