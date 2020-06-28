62364d5af63236423c69905c36659cbe06e5cad1
Karthick Narayanan (knaraya2) <knaraya2@cisco.com>
2020-06-28 19:45:37 +0530

    commit

--- problemsolving/8.coins_atleast.cpp
+++ problemsolving/8.coins_atleast.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 
+int solve_m(std::vector<int> &v, std::vector<int> &coins,int m){
+
+  if(v[m] != INT_MAX) {
+    return v[m];
+  }
+
+  for(auto c : coins) {
+    if(m-c){
+      v[m] = std::min(v[m], solve_m(v, coins, m-c)+1);
+    }
+  }
+  return v[m];
+}
 
 int main(){
   std::vector<int> v;
@@ -17,12 +30,14 @@ int main(){
     v[t] = 1;
   }
 
-  for(int i=1; i<v.size(); i++){
+  /* we are calculating for all possible combiatnions. lets calculate only what we want.
+    for(int i=1; i<v.size(); i++){
     for(int j=0;j<coins.size(); j++) {
       if(i-coins[j] > 0) {
         v[i] = std::min(v[i],v[i-coins[j]]+1);
       }
     }
-  }
+  }*/
+  solve_m(v, coins, m);
   std::cout<<v[m];
 }
