d1505f438e570b7e384b049017b42ce1ab7b396f
Karthick Narayanan (knaraya2) <knaraya2@cisco.com>
2020-06-29 21:34:51 +0530

    Levenshtein Distance.

---
+++ problemsolving/12.Levenshtein_distance.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+int cost(std::string &str1, std::string &str2,int str1_st, int str2_st){
+  if (str1_st >= str1.length()){
+    return 1;
+  }
+  if (str2_st >= str2.length()) {
+    return 1;
+  }
+  if(str1[str1_st] == str2[str2_st]){
+    return 0;
+  }
+  return 1;
+}
+
+int find_distance(std::string &str1, std::string &str2,int str1_st, int str2_st){
+  if(str1_st >= str1.length() && str2_st >= str2.length()){
+    return 0;
+  }
+  if(str1_st == str1.length()){
+    return str2.length() - str2_st;
+  }
+  if(str2_st == str2.length()) {
+    return str1.length() - str1_st;
+  }
+
+  return std::min(std::min(find_distance(str1, str2, str1_st + 1, str2_st) + 1,
+                  find_distance(str1, str2, str1_st, str2_st +1) + 1),
+                  find_distance(str1, str2, str1_st + 1, str2_st +1) + cost(str1, str2, str1_st, str2_st));
+
+}
+
+int main() {
+  std::string str1 = "LOVE";
+  std::string str2 = "MOVEEEEEE";
+
+  std::cout<<find_distance(str1, str2, 0, 0)<<"\n";
+
+}
